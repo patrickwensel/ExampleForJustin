8135f46700f66f1208b80b68a3e963fcbf033bc0
patrickwensel <pwensel@hotmail.com>
2016-02-25 12:04:05 -0500

    Initial Upload
    Initial Upload

---
+++ ApplicationsMembershipSecurity/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApplicationsMembershipSecurity.MvcApplication" Language="C#" %>
---
+++ ApplicationsMembershipSecurity/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ApplicationsMembershipSecurity.Startup))]
+namespace ApplicationsMembershipSecurity
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
